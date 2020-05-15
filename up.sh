8393af5da7788ebde56ef6eb68e7c24c74cea976
jonahjon <jonahjones094@gmail.com>
2020-05-15 11:29:58 -0400

    adding images

--- up.sh
+++ up.sh
@@ -24,7 +24,10 @@ kubectl apply -f namespace.yaml
 helm upgrade -i prometheus stable/prometheus \
     --namespace monitoring \
     --set alertmanager.persistentVolume.storageClass="gp2" \
-    --set server.persistentVolume.storageClass="gp2"
+    --set server.persistentVolume.storageClass="gp2" \
+    --set server.service.type=LoadBalancer \
+    --set alertmanager.service.type=LoadBalancer
+
 
 helm upgrade -i grafana ./grafana \
     --namespace monitoring \
@@ -38,6 +41,26 @@ helm upgrade -i grafana ./grafana \
     --set datasources."datasources\.yaml".datasources[0].isDefault=true \
     --set service.type=LoadBalancer
 
+helm upgrade -i grafana stable/grafana \
+    --namespace monitoring \
+    --set persistence.storageClassName="gp2" \
+    --set adminPassword='password123' \
+    --set datasources."datasources\.yaml".apiVersion=1 \
+    --set datasources."datasources\.yaml".datasources[0].name=Prometheus \
+    --set datasources."datasources\.yaml".datasources[0].type=prometheus \
+    --set datasources."datasources\.yaml".datasources[0].url=http://prometheus-server.monitoring.svc.cluster.local \
+    --set datasources."datasources\.yaml".datasources[0].access=proxy \
+    --set datasources."datasources\.yaml".datasources[0].isDefault=true \
+    --set dashboardProviders."dashboardproviders\.yaml".apiVersion=1 \
+    --set dashboardProviders."dashboardproviders\.yaml".providers[0].name=default \
+    --set dashboardProviders."dashboardproviders\.yaml".providers[0].orgId=1 \
+    --set dashboardProviders."dashboardproviders\.yaml".providers[0].folder="" \
+    --set dashboardProviders."dashboardproviders\.yaml".providers[0].type=file \
+    --set dashboardProviders."dashboardproviders\.yaml".providers[0].disableDeletion=false \
+    --set dashboardProviders."dashboardproviders\.yaml".providers[0].editable=true \
+    --set dashboardProviders."dashboardproviders\.yaml".providers[0].options.path=/var/lib/grafana/dashboards/default \
+    --set dashboards.default.elb.url="https://raw.githubusercontent.com/jonahjon/promcat/master/grafana/dashboards/elb_dashboard.json" \
+    --set service.type=LoadBalancer
 
 kubectl apply -f yace/
 
